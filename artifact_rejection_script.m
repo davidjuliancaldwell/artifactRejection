80ca69152699270a934af2033d1f6de1dcaf950b
David Caldwell <djcald@cs.washington.edu>
2018-02-23 10:02:06 -0800

    added in more comments, and removed an additional older function

--- artifact_rejection_script.m
+++ artifact_rejection_script.m
@@ -10,14 +10,14 @@ dataChoice = 1;
 switch dataChoice
         case 1
         load('693ffd_exampData_800ms.mat') % response timing data set
-        train_duration = [0 800];
-        xlims = [-200 1000];
-        chanIntList = [12 21 28 19 18 36 44 43 30 33 41 34];
+        train_duration = [0 800]; % this is how long the stimulation train was
+        xlims = [-200 1000]; % these are the x limits to visualize in plots
+        chanIntList = [12 21 28 19 18 36 44 43 30 33 41 34]; % these are the channels of interest to visualize in closer detail
     case 2
         load('2fd831_exampData_400ms.mat') % response timing data set
-        train_duration = [0 400];
-        xlims = [-200 1000];
-        chanIntList = [2 10 51 42];
+        train_duration = [0 400]; % this is how long the stimulation train was
+        xlims = [-200 1000]; % these are the x limits to visualize in plots
+        chanIntList = [2 10 51 42]; % these are the channels of interest to visualize in closer detail
     case 3
         load('50ad9_paramSweep4.mat') % DBS data set
     case 4
@@ -45,8 +45,6 @@ end
 %% linear interpolation with simple linear interpolation scheme
 
 % process the signal
-%pre_interp
-%post_interp
 
 % the type variable here determines whether to use a linear interpolation
 % scheme or a polynomial spline interpolation scheme
@@ -56,8 +54,18 @@ type = 'linear';
 % stimulation onset, or to detect the end of each pulse
 useFixedEnd = 1; 
 
+% this is how far to look before the algorithm detects each stimulation
+% pulse onset to allow for maximal artifact rejection
 pre = 0.4096; % in ms
+
+% this is how far to look after the algorithm detects each stimulation
+% pulse onset to allow for maximal artifact rejection
 post = 0.4096; % in ms
+
+% This is the maximal duration of time allowed for the artifact rejection
+% for each pulse, and if using "useFixedEnd", it simply considers this time
+% window. Otherwise, the algorithm detects the individual offset of each
+% stimulation pulse. 
 fixed_distance = 2.2; % in ms
 
 % perform the processing
@@ -67,6 +75,9 @@ processedSig = interpolate_artifact(dataInt,'fs',fs_data,'plotIt',0,'type',type,
 % visualization
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 
+% multiple visualizations are created with this call, including time domain
+% and spectral analyses
+
 multiple_visualizations(processedSig,dataInt,'fs_data',fs_data,'type',type,'t_epoch',...
     t_epoch,'xlims',xlims,'train_duration',train_duration,'stimChans',stimChans,...,
     'chanIntList',chanIntList)
@@ -75,6 +86,7 @@ multiple_visualizations(processedSig,dataInt,'fs_data',fs_data,'type',type,'t_ep
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 %% linear interpolation with polynomial piecewise interpolation
 
+% using piecewise polynomial interpolation here 
 type = 'pchip';
 useFixedEnd = 1;
 pre = 0.4096; % in ms
@@ -105,17 +117,27 @@ multiple_visualizations(processedSig,dataInt,'fs_data',fs_data,'type',type,'t_ep
 % post is the equivalent for after the offset of the stimulus pulse
 % fs_data is the sampling frequency in Hz
 
-% most recent - 0.9, 1.8
+% this determines the type of template approach to use. The three options
+% are 'average', 'trial', and 'dictionary'. Average is the simplest
+% approach, and on a channel by channel basis it simply averages the
+% artifact for each channel. Trial uses the stimulation pulse train within
+% each trial. 'Dictionary' is the most advanced, and uses a template
+% matching algorithm with DBSCAN clustering to discover the family of
+% artifacts. 
 
 type = 'trial';
 useFixedEnd = 1;
 fixed_distance = 2.8; % in ms
 
-pre = 0.4096; % in ms
-post = 0.4096; % in ms
+%pre = 0.4096; % in ms
+%post = 0.4096; % in ms
 
 pre = 1; % started with 0.7, then 0.9, adjusted the window, move it to 0.4
 post = 0.2; % started with 0.7, then 1.1, then 1.5, then 1.8
+
+% these are the metrics used if the dictionary method is selected. The
+% options are 'eucl', 'cosine', 'corr', for either euclidean distance,
+% cosine similarity, or correlation for clustering and template matching. 
 
 distance_metric_dbscan = 'eucl';
 distance_metric_sigMatch = 'eucl';
@@ -124,6 +146,8 @@ distance_metric_sigMatch = 'eucl';
     'distance_metric_dbscan',distance_metric_dbscan,'distance_metric_sigMatch',distance_metric_sigMatch);
 
 % visualization
+% of note - more visualizations are created here, including what the
+% templates look like on each channel, and what the discovered templates are
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 multiple_visualizations(processedSig,dataInt,'fs_data',fs_data,'type',type,'t_epoch',...
     t_epoch,'xlims',xlims,'train_duration',train_duration,'stimChans',stimChans,...,
